1b9ea0aa36fec9f3eef65284acfa48bb15052cfa
Mahmudul Hasan <mahmudnotes@outlook.com>
2020-09-21 15:58:57 +0600

    added multi operation support

--- src/Calculator/Calculator.java
+++ src/Calculator/Calculator.java
@@ -1,297 +1,192 @@
 package Calculator;
-import com.sun.source.tree.WhileLoopTree;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 
 public class Calculator {
-    private JPanel mainform;
-    private JButton a4Button;
-    private JButton a5Button;
-    private JButton a6Button;
-    private JButton subbtn;
-    private JButton a7Button;
-    private JButton CEButton;
-    private JButton modbtn;
-    private JButton divbtn;
-    private JButton a8Button;
-    private JButton a9Button;
-    private JButton xButton;
+    private JPanel mainForm;
     private JFormattedTextField display;
-    private JButton addbtn;
-    private JButton equalbtn;
-    private JButton a3Button;
-    private JButton dotbtn;
-    private JButton a2Button;
-    private JButton a1Button;
-    private JButton plusminsign;
-    private JButton zerobtn;
-    private JButton cbtn;
+    private JButton a4Button, a5Button, a6Button, subBtn;
+    private JButton a7Button, CEButton, modBtn, divBtn, a8Button, a9Button, xButton;
+    private JButton addBtn, equalBtn, a3Button;
+    private JButton dotBtn, a2Button, a1Button, plusMinSign;
+    private JButton zeroBtn, cBtn, openingParenthesis, closingParenthesis;
     static StringBuilder displayText = new StringBuilder();
+    Stack<String> bracketStack = new Stack<String>();
 
     public Calculator() {
         this.display.setComponentOrientation(ComponentOrientation.LEFT_TO_RIGHT);
-        dotbtn.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                if (displayText.indexOf(".") == -1) {
-                    displayText.append(".");
-                    display.setText(String.valueOf(displayText));
-                }
-            }
-        });
-        plusminsign.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                if (displayText.charAt(0) == '-') {
-                    displayText.deleteCharAt(0);
-                } else {
-                    displayText.insert(0, '-');
-                }
-                display.setText(String.valueOf(displayText));
-            }
-        });
-        modbtn.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                if (displayText.indexOf("%") == -1 && displayText.indexOf("+") == -1 &&
-                        displayText.indexOf("-") == -1 && displayText.indexOf("x") == -1 &&
-                        displayText.indexOf("*") == -1 && displayText.indexOf("/") == -1) {
-                    displayText.append("%");
-                    display.setText(String.valueOf(displayText));
-                }
-            }
-        });
-        divbtn.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                if (displayText.indexOf("%") == -1 && displayText.indexOf("+") == -1 &&
-                        displayText.indexOf("-") == -1 && displayText.indexOf("x") == -1 &&
-                        displayText.indexOf("*") == -1 && displayText.indexOf("/") == -1) {
-                    displayText.append("/");
-                    display.setText(String.valueOf(displayText));
-                }
-            }
-        });
-        xButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                if (displayText.indexOf("%") == -1 && displayText.indexOf("+") == -1 &&
-                        displayText.indexOf("-") == -1 && displayText.indexOf("x") == -1 &&
-                        displayText.indexOf("*") == -1 && displayText.indexOf("/") == -1) {
-                    displayText.append("x");
-                    display.setText(String.valueOf(displayText));
-                }
-            }
-        });
-        subbtn.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                if (displayText.indexOf("%") == -1 && displayText.indexOf("+") == -1 &&
-                        displayText.indexOf("-") == -1 && displayText.indexOf("x") == -1 &&
-                        displayText.indexOf("*") == -1 && displayText.indexOf("/") == -1) {
-                    displayText.append("-");
-                    display.setText(String.valueOf(displayText));
-                }
-            }
-        });
-        addbtn.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                if (displayText.indexOf("%") == -1 && displayText.indexOf("+") == -1 &&
-                        displayText.indexOf("-") == -1 && displayText.indexOf("x") == -1 &&
-                        displayText.indexOf("*") == -1 && displayText.indexOf("/") == -1) {
-                    displayText.append("+");
-                    display.setText(String.valueOf(displayText));
-                }
-            }
-        });
-        CEButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                try {
-                    displayText.deleteCharAt(displayText.length() - 1);
-                    display.setText(String.valueOf(displayText));
-                } catch (Exception exception) {
-                }
 
-            }
-        });
-        cbtn.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                displayText.setLength(0);
+        //  Adds a Decimal Point in the expression
+        dotBtn.addActionListener(e -> {
+            if (displayText.indexOf(".") == -1) {
+                displayText.append(".");
                 display.setText(String.valueOf(displayText));
             }
         });
-        zerobtn.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                displayText.append("0");
+
+        // Adds plus or minus sign at the end
+        plusMinSign.addActionListener(e -> {
+            if (displayText.charAt(0) == '-')
+                displayText.deleteCharAt(0);
+            else
+                displayText.insert(0, '-');
+            display.setText(String.valueOf(displayText));
+        });
+
+        // Delete Button
+        CEButton.addActionListener(e -> {
+            try {
+                displayText.deleteCharAt(displayText.length() - 1);
                 display.setText(String.valueOf(displayText));
+            } catch (Exception ignored) {
+            }
+
+        });
+
+        // Clear Button
+        cBtn.addActionListener(e -> {
+            displayText.setLength(0);
+            display.setText(String.valueOf(displayText));
+        });
+
+        // Char assignment
+        modBtn.addActionListener(e -> changeDisplay("%"));
+        divBtn.addActionListener(e -> changeDisplay("/"));
+        xButton.addActionListener(e -> changeDisplay("*"));
+        subBtn.addActionListener(e -> changeDisplay("-"));
+        addBtn.addActionListener(e -> changeDisplay("+"));
+        zeroBtn.addActionListener(e -> changeDisplay("0"));
+        a1Button.addActionListener(e -> changeDisplay("1"));
+        a2Button.addActionListener(e -> changeDisplay("2"));
+        a3Button.addActionListener(e -> changeDisplay("3"));
+        a4Button.addActionListener(e -> changeDisplay("4"));
+        a5Button.addActionListener(e -> changeDisplay("5"));
+        a6Button.addActionListener(e -> changeDisplay("6"));
+        a7Button.addActionListener(e -> changeDisplay("7"));
+        a8Button.addActionListener(e -> changeDisplay("9"));
+        a9Button.addActionListener(e -> changeDisplay("9"));
+        openingParenthesis.addActionListener(e -> {
+            if (displayText.length() == 0 || displayText.charAt(displayText.length() - 1) != ')') {
+                bracketStack.push("(");
+                changeDisplay("(");
             }
         });
-        a1Button.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                displayText.append("1");
-                display.setText(String.valueOf(displayText));
+        closingParenthesis.addActionListener(e -> {
+            if (!bracketStack.isEmpty()) {
+                bracketStack.pop();
+                changeDisplay(")");
             }
         });
-        a2Button.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                displayText.append("2");
-                display.setText(String.valueOf(displayText));
-            }
-        });
-        a3Button.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                displayText.append("3");
-                display.setText(String.valueOf(displayText));
-            }
-        });
-        a4Button.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                displayText.append("4");
-                display.setText(String.valueOf(displayText));
-            }
-        });
-        a5Button.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                displayText.append("5");
-                display.setText(String.valueOf(displayText));
-            }
-        });
-        a6Button.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                displayText.append("6");
-                display.setText(String.valueOf(displayText));
-            }
-        });
-        a7Button.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                displayText.append("7");
-                display.setText(String.valueOf(displayText));
-            }
-        });
-        a8Button.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                displayText.append("8");
-                display.setText(String.valueOf(displayText));
-            }
-        });
-        a9Button.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                displayText.append("9");
-                display.setText(String.valueOf(displayText));
-            }
-        });
+
+        // Keyboard Input Handling
         display.addKeyListener(new KeyAdapter() {
-            @Override
             public void keyTyped(KeyEvent e) {
                 char c = e.getKeyChar();
-                if (!((c >= '0' && c <= '9') || c == '+' || c == '-' || c == 'x' || c == '*' || c == '/' || c == '%')) {
+                char[] legalChar = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '%', '-', '+', '/', '*', '(', ')'};
+                if (!(new String(legalChar).contains(String.valueOf(c))))
                     e.consume();
-                }
                 displayText = new StringBuilder(display.getText());
             }
         });
-        equalbtn.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                try {
-                    String DivMulPattern = "/([0-9]*\\.?[0-9]*)([\\*/])([0-9]*\\.?[0-9]*)/";
-                    String AddSubPattern = "/([0-9]*\\.?[0-9]*)([\\+\\-])([0-9]*\\.?[0-9]*)/";
 
-                    Double t;
-                    String[] s;
-                    /*
-                    try{
-                        Pattern r = Pattern.compile(DivMulPattern);
-                        Matcher m = r.matcher(display.getText());
-                        if (m.group(2) == "*" || m.group(2) == "x") {
-                            s = displayText.toString().split("\\*");
-                            t = Double.parseDouble(s[0]) * Double.parseDouble(s[1]);
-                        }
-                        else {
-                            s = displayText.toString().split("/");
-                            t = Double.parseDouble(s[0]) / Double.parseDouble(s[1]);
-                        }
-                    }
-                    catch (Exception exception){display.setText(exception.getMessage());}
-                    try{
-                        Pattern r = Pattern.compile(AddSubPattern);
-                        Matcher m = r.matcher(display.getText());
-                        if (m.group(2) == "+") {
-                            s = displayText.toString().split("\\*");
-                            t = Double.parseDouble(s[0]) + Double.parseDouble(s[1]);
-                        }
-                        else
-                        {
-                            s = displayText.toString().split("-");
-                            t = Double.parseDouble(s[0]) - Double.parseDouble(s[1]);
-                        }
-                    }
-                    catch (Exception exception){display.setText(exception.getMessage());}
-                    */
-
-                    if (displayText.indexOf("+") != -1) {
-                        s = displayText.toString().split("\\+");
-                        t = Double.parseDouble(s[0]) + Double.parseDouble(s[1]);
-                    } else if (displayText.indexOf("-") != -1) {
-                        s = displayText.toString().split("-");
-                        t = Double.parseDouble(s[0]) - Double.parseDouble(s[1]);
-                    } else if (displayText.indexOf("/") != -1) {
-                        s = displayText.toString().split("/");
-                        t = Double.parseDouble(s[0]) / Double.parseDouble(s[1]);
-                    } else if (displayText.indexOf("%") != -1) {
-                        s = displayText.toString().split("%");
-                        t = Double.parseDouble(s[0]) % Double.parseDouble(s[1]);
-
-                    } else if (displayText.indexOf("x") != -1) {
-                        s = displayText.toString().split("x");
-                        t = Double.parseDouble(s[0]) * Double.parseDouble(s[1]);
-
-                    } else if (displayText.indexOf("*") != -1) {
-                        s = displayText.toString().split("\\*");
-                        t = Double.parseDouble(s[0]) * Double.parseDouble(s[1]);
-
-                    } else {
-                        t = Double.parseDouble(display.getText());
-                    }
-
-                    if (t % 1 == 0) {
-                        displayText = new StringBuilder(String.valueOf(t.intValue()));
-                    } else
-                        displayText = new StringBuilder(String.valueOf(t));
-                    display.setText(String.valueOf(displayText));
-                } catch (Exception exception) {
-                    display.setText(exception.getMessage());
-                }
+        // Result Button
+        equalBtn.addActionListener(e -> {
+            try {
+                if (displayText.charAt(0) != '(')
+                    displayText.insert(0, '(');
+                if (displayText.charAt(displayText.length() - 1) != ')')
+                    displayText.append(")");
+                double res = calculator((displayText.toString()).split(""));
+                displayText.setLength(0);
+                if (res % 1 == 0)
+                    displayText.append((int) res);
+                else
+                    displayText.append(res);
+                display.setText(String.valueOf(displayText));
+                displayText.setLength(0);
+            } catch (Exception ignored) {
             }
         });
+    }
+
+    // Display changing function
+    private void changeDisplay(String ch) {
+        displayText.append(ch);
+        display.setText(String.valueOf(displayText));
+    }
+
+    // Calculator Engine
+    public static double calculator(String[] strArr) {
+        Stack<String> operators = new Stack<String>();
+        Stack<Double> operands = new Stack<Double>();
+        StringBuilder temp = new StringBuilder();
+        for (String str : strArr) {
+            if (str.trim().equals("")) {
+                continue;
+            }
+
+            switch (str) {
+                case "(":
+                    break;
+                case ")":
+                    if (temp.length() > 0) {
+                        operands.push(Double.parseDouble(temp.toString()));
+                        temp.setLength(0);
+                    }
+                    double right = operands.pop();
+                    double left = operands.pop();
+                    String operator = operators.pop();
+                    double value = 0;
+                    switch (operator) {
+                        case "+":
+                            value = left + right;
+                            break;
+                        case "-":
+                            value = left - right;
+                            break;
+                        case "*":
+                            value = left * right;
+                            break;
+                        case "/":
+                            value = left / right;
+                            break;
+                        case "%":
+                            value = left % right;
+                            break;
+                        default:
+                            break;
+                    }
+                    operands.push(value);
+                    break;
+                case "+":
+                case "-":
+                case "*":
+                case "/":
+                case "%":
+                    if (temp.length() > 0) {
+                        operands.push(Double.parseDouble(temp.toString()));
+                        temp.setLength(0);
+                    }
+                    operators.push(str);
+                    break;
+                default:
+                    temp.append(str);
+                    break;
+            }
+        }
+
+        return operands.pop();
     }
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("Calculator");
         frame.setResizable(false);
-        frame.setContentPane(new Calculator().mainform);
+        frame.setContentPane(new Calculator().mainForm);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
-   } 
+    }
 }
